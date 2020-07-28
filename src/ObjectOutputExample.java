7720c3479d6fe592693bf6a15082c1c1e80758e1
Trungdeptrai99 <ktdarkness99@gmail.com>
2020-07-28 09:12:25 +0700

    [Optional] [Bài tập] Copy file nhị phân

--- src/ObjectOutputExample.java
+++ src/ObjectOutputExample.java
@@ -4,7 +4,7 @@ public class ObjectOutputExample {
         try {
             FileOutputStream fos = new FileOutputStream("Data/Data2.txt");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
-            Stock s[] = {
+            Stock[] s = {
                     new Stock(1, "CD ROM", 100, 2)
                     ,new Stock(2, "CPU", 500, 2)
                     ,new Stock(3, "HP Scanner", 75, 1)
